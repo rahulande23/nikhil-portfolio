a50086caa89bdddb88ce83427c36a651b32b6e26
rahulande23-dev <rahul@gnssoftware.in>
2026-04-08 11:33:54 +0530

    first commit

---
+++ links.js
@@ -0,0 +1,17 @@
+const clothing_brand_intro = "https://drive.google.com/file/d/1dEgvBva5l-c_LSlobyZc-xj2xKCJHPtb/view?usp=sharing";
+
+const compressed_paytm_reel_5_by_rahul = "https://drive.google.com/file/d/1hSNcJHAGCpsBRzsOh7K02VT_eEZCAW8y/view?usp=sharing";
+
+const testing_2 = "https://drive.google.com/file/d/18GlNdhSf9vy9nU5ryGNUQeVdZvrxSY4l/view?usp=sharing";
+
+const villa_final = "https://drive.google.com/file/d/1bcVFdzCYs8JAh82-QZHxnASMHgEUOWMk/view?usp=sharing";
+
+const wonderla_edit_landscape = "https://drive.google.com/file/d/1WWm5PoEeUbIJAqawbPKbefPfPS7FoQ9p/view?usp=sharing";
+
+const yuva_sangam_phase_6__1 = "https://drive.google.com/file/d/1dc7g37L2n5FgpIrn18LHYJNPaY7aVswA/view?usp=sharing";
+
+const foreign_1_video_edited = "https://drive.google.com/file/d/15DdIsQUlKn93WLlMHQIJ94qp1g5iGLYu/view?usp=sharing";
+
+const kolkata_blur = "https://drive.google.com/file/d/1bJrwOxHdRqP9z7W5aTepOOJis7cVfJsD/view?usp=sharing";
+
+const promo_final = "https://drive.google.com/file/d/1LobORJtqo3YqtHAdxLEGbzlh_CCahCTF/view?usp=sharing";
